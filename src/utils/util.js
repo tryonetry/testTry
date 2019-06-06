6f0b14951b9534ab7e4359c8942696a6d6ca7211
sunshine <2934735161@qq.com>
2019-06-06 15:23:15 +0800

    new

--- src/utils/util.js
+++ src/utils/util.js
@@ -124,7 +124,32 @@ const utils = {
             tmpNode.parentNode.removeChild(tmpNode);
         }
         return arrDPI;
-   } 
+    },
+
+    // 监听浏览器缩放比例
+    detectZoom (){
+        var ratio = 0,
+            screen = window.screen,
+            ua = navigator.userAgent.toLowerCase();
+        
+        if (window.devicePixelRatio !== undefined) {
+            ratio = window.devicePixelRatio;
+        }
+        else if (~ua.indexOf('msie')) { 
+          if (screen.deviceXDPI && screen.logicalXDPI) {
+            ratio = screen.deviceXDPI / screen.logicalXDPI;
+          }
+        }
+        else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+          ratio = window.outerWidth / window.innerWidth;
+        }
+          
+         if (ratio){
+          ratio = Math.round(ratio * 100);
+        }
+          
+         return ratio;
+      }
 
 }
 export default utils;
