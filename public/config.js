e7a07deaa57e81f5072cf74b248cc73db706e4a2
sunshine <2934735161@qq.com>
2019-11-04 09:48:17 +0800

    new--忽略

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 var  ApiUrl = {};
 ApiUrl.targetHost = 'http://192.168.1.215:8181/';
 ApiUrl.readAddress = '192.168.1.215';   //阅读器主机
-ApiUrl.proxyAddress = '192.168.1.215:8181';  //解决代理跨域地址
+ApiUrl.proxyAddress = '192.168.1.215:8181';  //解决代理跨域地址--目前解决不了
 //在http.js 当对象用
 export {
     ApiUrl
